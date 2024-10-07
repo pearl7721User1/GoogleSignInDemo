559aee6c26930995180f83d2590dc50b77ee9aa7
giwon.seo <giwon.seo@neolab.net>
2024-10-07 10:25:01 +0900

    fix to meet app route requirement

--- google-sign-in-demo/src/app/api/.well-known/apple-app-site-association.js
+++ google-sign-in-demo/src/app/api/.well-known/apple-app-site-association.js
@@ -1,16 +1,23 @@
-// pages/api/.well-known/apple-app-site-association.js
-export default function handler(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-      applinks: {
-        apps: [],
-        details: [
-          {
-            appID: "K2APH36C6M.com.UniversalLinkTestApp", // Replace with your app's team ID and bundle ID
-            paths: ["/dashboard"]
-          }
-        ]
-      }
-    });
-  }
+// app/api/.well-known/apple-app-site-association/route.js
+export async function GET(req) {
+  const response = {
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: "K2APH36C6M.com.UniversalLinkTestApp", // Replace with your app's team ID and bundle ID
+          paths: ["/dashboard"], // Define the paths you want to handle
+        },
+      ],
+    },
+  };
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
   
